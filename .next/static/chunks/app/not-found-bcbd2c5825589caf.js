6fed84ce12e50f33552c709fc7264cc419c2d698
Hafidz25 <hafidzalif80@gmail.com>
2023-10-21 16:43:51 +0700

    fix: bug

--- .next/static/chunks/app/not-found-bcbd2c5825589caf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{2531:function(e,n,s){Promise.resolve().then(s.t.bind(s,2575,23)),Promise.resolve().then(s.t.bind(s,3235,23))}},function(e){e.O(0,[876,701,744],function(){return e(e.s=2531)}),_N_E=e.O()}]);
